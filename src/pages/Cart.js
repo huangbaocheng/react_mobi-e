6821b2527ef535be7e3e8b4e1eda88d1c9e4f787
huangbaocheng <786886565@qq.com>
2019-07-19 10:06:15 +0800

    重新打开项目有问题

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,10 +2,10 @@
 
 
 import React, { Component, Fragment } from "react";
-import { NavBar, Icon } from "antd-mobile";
+import { NavBar, Icon, List, SwipeAction,Checkbox} from "antd-mobile";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { SwipeAction, List } from "antd-mobile";
+const CheckboxItem = Checkbox.CheckboxItem;
 
 
 class Cart extends Component {
@@ -25,33 +25,96 @@ class Cart extends Component {
         </NavBar>
         {/* 顶部导航栏结束 */}
 
-        {/* 中间图文开始 */}
-        <SwipeAction
-          style={{ backgroundColor: "gray" }}
-          autoClose
-          right={[
+        {/* 购物车列表开始 */}
+        <div className="cart_content">
+          {this.props.carts.map(v => (
+            <div className="cart_item" key={v.id}>
+              <List>
+                <SwipeAction
+                  style={{ backgroundColor: "gray" }}
+                  autoClose
+                  right={[
+                    {
+                      text: "取消",
+                      onPress: () => console.log("cancel"),
+                      style: { backgroundColor: "#ddd", color: "white" }
+                    },
+                    {
+                      text: "删除",
+                      onPress: () => console.log("delete"),
+                      style: { backgroundColor: "#F4333C", color: "white" }
+                    }
+                  ]}
+                  onOpen={() => console.log("global open")}
+                  onClose={() => console.log("global close")}
+                >
+                  <div className="cart_inner">
+                    {/* 1复选框开始 */}
+                    <div className="goods_chk_wrap">
+                      <CheckboxItem>{v.isChecked}</CheckboxItem>
+                    </div>
+                    {/* 1复选框结束 */}
+
+                    {/* 2商品图片开始 */}
+                    <div className="goods_img_wrap">
+                      <img src={v.img_url} alt="" />
+                    </div>
+                    {/* 2商品图片结束 */}
+                    {/* 3商品名称开始*/}
+                    <div className="goods_name_wrap">
+                      <p>{v.goods_name}</p>
+                      <p>{v.price}</p>
+                    </div>
+                    {/* 3商品名称结束 */}
+                    {/* 4商品数量开始 */}
+                    <div className="goods_num_wrap">
+                      <span className="iconfont icon-minus" />
+                      <span>{v.num}</span>
+                      <span className="iconfont icon-plus" />
+                    </div>
+                    {/* 4商品数量结束 */}
+                  </div>
+                </SwipeAction>
+              </List>
+            </div>
+          ))}
+          <style jsx>
             {
-              text: "取消",
-              onPress: () => console.log("cancel"),
-              style: { backgroundColor: "#ddd", color: "white" }
-            },
-            {
-              text: "删除",
-              onPress: () => console.log("delete"),
-              style: { backgroundColor: "#F4333C", color: "white" }
+              `
+              
+
+
+
+
+    .cart_content{
+        .cart_item{
+            .cart_inner{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                .goods_chk_wrap{
+                    flex: 1
+                }
+                .goods_img_wrap{
+                    flex: 3;
+                 
+                    
+                }
+                .goods_name_wrap{
+                    flex: 2
+                }
+                .goods_num_wrap{
+                    flex: 2
+                }
             }
-          ]}
-        
-        >
-          <List.Item
-            extra="More"
-            arrow="horizontal"
-            onClick={e => console.log(e)}
-          >
-            Have left and right buttons
-          </List.Item>
-        </SwipeAction>
-        {/* 中间图文结束 */}
+        }
+    }
+              `
+            }
+
+          </style>
+        </div>
+        {/* 购物车列表结束 */}
       </Fragment>
     );
   }
